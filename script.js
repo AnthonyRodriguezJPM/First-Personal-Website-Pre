91c4702095e28fa276fb7861bbf31b983576c298
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-08-01 02:18:23 -0500

    Dog Story

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Created by: Anthony Rodriguez";
 
 document.querySelector("body").appendChild(h2);
 
 
-//
+const input = document.getElementById('myBtn');
+
+function clickAlert() {
+  alert('Woof Wooooof!!');
+}
+
+input.addEventListener('click', clickAlert);
+
+clickAlert(e);
